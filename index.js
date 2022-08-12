feb0d943144e651aeb89e7e900ce038c55a68103
johnyrock <johnreyroco@vista.aero>
2022-08-12 10:35:43 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// const cors = require('cors')
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+
+const app = express();
+
+const PORT = 8080;
+// const HOST = "localhost";
+
+// app.use(cors())
+
+app.get('/info', (req, res, next) => {
+    res.send('Proxy Service Test 2');
+ });
+
+app.listen(PORT, () => {
+    console.log(`Starting Proxy at ${PORT}`);
+ });
+
+//  exports.app = functions.https.onRequest(app);
