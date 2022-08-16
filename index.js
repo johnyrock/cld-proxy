3a6fc1a6bee57fadb1d852b6447e7e01165a51c3
johnyrock <johnreyroco@vista.aero>
2022-08-16 15:06:30 +0800

    add signature function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const cors = require('cors')
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
-
+const apiSecret = cloudinary.config().api_secret;
 
 
 const app = express();
@@ -15,15 +15,43 @@ const PORT = 8080;
 app.get('/info', (req, res, next) => {
     // console.log(cloudinary.url("samples/cloudinary-group.jpg"));
     // res.send('test');
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    );
+
     res.json({
         number: 1,
         name: 'John',
         gender: 'male'
-      });
- });
+    });
+});
+
+app.get('/signature', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    );
+    
+    const timestamp = Math.round((new Date).getTime() / 1000);
+
+    const signature = cloudinary.utils.api_sign_request({
+        timestamp: timestamp,
+        source: 'uw',
+        folder: 'samples'
+    }, apiSecret);
+
+    // return { timestamp, signature }
+    res.json({
+        timestamp: timestamp,
+        signature: signature
+    });
+})
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Starting Proxy at ${PORT}`);
- });
+});
 
 //  exports.app = functions.https.onRequest(app);
